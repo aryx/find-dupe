4595e8f90f086348402f3c297d3cbe233379214b
pad <yoann.padioleau@gmail.com>
2018-05-01 20:08:25 -0700

    * main.ml: v3 optimized with size

--- main.ml
+++ main.ml
@@ -1,14 +1,13 @@
 open Common
 
-let signatures files = 
-  files |> List.map (fun file ->
-    match Common.cmd_to_list (spf "md5sum %s" (Filename.quote file)) with
-    | [x] -> 
-      if x =~ "^\\([0-9a-f]+\\) .*$"
-      then Common.matched1 x, file
-      else failwith (spf "not a md5sum signature %s for %s" x file)
-    | _ -> failwith (spf "could not get signature for %s" file)
-  )
+let signature file = 
+  match Common.cmd_to_list (spf "md5sum %s" (Filename.quote file)) with
+  | [x] -> 
+    if x =~ "^\\([0-9a-f]+\\) .*$"
+    then Common.matched1 x
+    else failwith (spf "not a md5sum signature %s for %s" x file)
+  | _ -> failwith (spf "could not get signature for %s" file)
+  
 
 let ask_delete dupe orig =
   pr2 (spf "file %s\n =   %s" dupe orig);
@@ -18,22 +17,49 @@ let main () =
   (* CONFIG ! *)
   let home = "/Users/luisa" in
   let dir1 = Filename.concat home "Downloads" in
-  let other_dirs = Filename.concat home "Documents" in
+  let other_dirs = Filename.concat home "Dropbox" in
 
   let files = 
     Common.cmd_to_list (spf "find %s -type f" dir1) in
-  let hfiles = signatures files |> Common.hash_of_list in
+  let hfiles = files |> List.map (fun file -> signature file, file)
+  |> Common.hash_of_list in
   pr2 (spf "Done scanning %s" dir1);
 
   let other_files = 
     Common.cmd_to_list (spf "find %s -type f" other_dirs) in
-  let hother = signatures other_files |> Common.hash_of_list in
+  (* use the Hashtbl.find_all property *)
+  let hother_size = Hashtbl.create 1001 in
 
+  other_files |> List.iter (fun file -> 
+    Hashtbl.add hother_size (Common2.filesize file) file
+  );
+  let hdone = Hashtbl.create 1001 in
+      
   pr2 (spf "Done scanning %s" other_dirs);
   
   hfiles |> Hashtbl.iter (fun k file ->
-    if Hashtbl.mem hother k
-    then ask_delete file (Hashtbl.find hother k)
+    let size = Common2.filesize file in
+    if Hashtbl.mem hother_size size && size > 100000
+    then begin 
+      let candidates = Hashtbl.find_all hother_size size in
+      match candidates |> Common.find_some_opt (fun candidate ->
+        let md5 = 
+          if Hashtbl.mem hdone candidate 
+          then Hashtbl.find hdone candidate
+          else begin
+            let md5 = signature candidate in
+            Hashtbl.add hdone candidate md5;
+            md5
+          end
+        in
+        if md5 = k
+        then Some (candidate, md5)
+        else None
+      ) with
+      | Some (orig, _) -> 
+        ask_delete file orig  
+      | None -> ()
+    end
   )
 
 let _ = main ()
